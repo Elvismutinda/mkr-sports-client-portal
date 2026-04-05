65382a452ca9f5f6ef0a18c717be6a9931b086d1
ElvisMutinda <elvis.mutinda@nextranssolutions.co.ke>
2026-04-05 11:30:29 +0300

    Switch to database url

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,10 +8,11 @@ export default defineConfig({
   strict: true,
   verbose: true,
   dbCredentials: {
-    password: env.DB_PASSWORD!,
-    user: env.DB_USER!,
-    database: env.DB_NAME!,
-    host: env.DB_HOST!,
-    ssl: false,
+    url: env.DATABASE_URL!,
+    // password: env.DB_PASSWORD!,
+    // user: env.DB_USER!,
+    // database: env.DB_NAME!,
+    // host: env.DB_HOST!,
+    // ssl: false,
   },
 })
--- lib/db/db.ts
+++ lib/db/db.ts
@@ -5,9 +5,10 @@ import * as schema from "./schema"
 export const db = drizzle({
   schema,
   connection: {
-    password: env.DB_PASSWORD,
-    user: env.DB_USER,
-    database: env.DB_NAME,
-    host: env.DB_HOST,
+    url: env.DATABASE_URL,
+    // password: env.DB_PASSWORD,
+    // user: env.DB_USER,
+    // database: env.DB_NAME,
+    // host: env.DB_HOST,
   },
 })
